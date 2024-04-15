c675c36764aa3ba40fbfecbab2bd20d25fc71948
yoniv <yoniv@gowther724>
2024-04-14 22:21:51 -0300

    entrega de la actividad 1

---
+++ tp1_JonatanIgnacioVasquez/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,12 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		short num1 = 5, num2 = 9, num3 = 3, num4 = 4, num5 = 12;
+		float promedio=(num1 + num2 + num3 + num4 + num5) / 5 ;
+		
+		System.out.println(promedio);
+	}
+
+}
